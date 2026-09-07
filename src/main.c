4004c3c5911c81ed1b821a5f2ee80497569fcb29
Ubuntu <ubuntu@localhost.localdomain>
2026-09-07 22:29:33 +0800

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -52,9 +52,19 @@ int main() {
 	printf("Waiting for a client to connect...\n");
 	client_addr_len = sizeof(client_addr);
 	
-	accept(server_fd, (struct sockaddr *) &client_addr, &client_addr_len);
-	printf("Client connected\n");
+	int client_fd = accept(server_fd, (struct sockaddr *) &client_addr, &client_addr_len);
+    if (client_fd == -1) {
+        printf("Accept failed: %s \n", strerror(errno));
+        close(server_fd);
+        return 1;
+	}
+    printf("Client connected\n");
 	
+    const char *response = "+PONG\r\n";
+	if (send(client_fd, response, strlen(response), 0) == -1) {
+		printf("Send failed: %s \n", strerror(errno));
+	}
+
 	close(server_fd);
 
 	return 0;
